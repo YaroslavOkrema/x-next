198560fe6239e76319e1b0c841b37ae330f9d402
YaroslavOkrema <yaroslav.okrema@gmail.com>
2025-12-28 01:53:26 +0200

    feat: useRouter

--- xfront/src/app/(public)/profile-fake/page.tsx
+++ xfront/src/app/(public)/profile-fake/page.tsx
@@ -1,3 +1,15 @@
+'use client'
+
+import {useRouter} from "next/navigation";
+import {PAGES} from "@/config/pages.config";
+
 export default function ProfileFakePage() {
-    return <div>Profile</div>
+    const router = useRouter()
+
+    return (
+        <div>
+            <h1 className="text-3xl font-bold mb-6">Profile fake</h1>
+            <button onClick={() => router.push(PAGES.HOME)}>Go to home</button>
+        </div>
+    )
 }
