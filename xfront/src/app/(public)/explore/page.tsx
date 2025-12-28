55e52c56b2366ce44d6dfd733029dbd14a933034
YaroslavOkrema <yaroslav.okrema@gmail.com>
2025-12-28 02:08:38 +0200

    feat: useSearchParams

--- xfront/src/app/(public)/explore/page.tsx
+++ xfront/src/app/(public)/explore/page.tsx
@@ -1,3 +1,17 @@
+'use client'
+
+import {useSearchParams} from "next/navigation";
+import {Suspense} from "react";
+
 export default function ExplorePage() {
-    return <div>Explore</div>
+    const searchParams = useSearchParams()
+    const tag = searchParams.get('tag')
+
+    return (
+        <Suspense>
+            <div>
+                <h1 className="text-3xl font-bold mb-6">Explore {!!tag && `by #${tag}`}</h1>
+            </div>
+        </Suspense>
+    )
 }
